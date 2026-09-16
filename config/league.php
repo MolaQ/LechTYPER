6e07578e7ddfd917946a3e1a47f3050df1f6bd37
MolaQ <marcin.molak@gmail.com>
2026-09-16 16:44:08 +0200

    feat: Add league configuration for points system and season settings

---
+++ config/league.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'points' => [
+        'win' => 3,
+        'draw' => 1,
+        'loss' => 0,
+    ],
+    'season_rounds' => 9,
+    'missed_selection_injury_days' => 14,
+];
