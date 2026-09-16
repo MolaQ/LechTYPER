b045edbb4474722cb1ae25fe02af18e09309e3cc
MolaQ <marcin.molak@gmail.com>
2026-09-16 16:43:20 +0200

    feat: Create Player model with fillable attributes and teamPlayers relationship

---
+++ app/Models/Player.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable(['name', 'position', 'is_active'])]
+class Player extends Model
+{
+    protected function casts(): array
+    {
+        return ['is_active' => 'boolean'];
+    }
+
+    public function teamPlayers(): HasMany
+    {
+        return $this->hasMany(TeamPlayer::class);
+    }
+}
