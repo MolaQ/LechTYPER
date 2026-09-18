e61e1f3bab0f931166ebfb6d91b67eaa14451d9c
MolaQ <marcin.molak@gmail.com>
2026-09-18 08:07:00 +0200

    feat: Add realMatch relationship to SeasonRound model

--- app/Models/SeasonRound.php
+++ app/Models/SeasonRound.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 #[Fillable(['season_id', 'round_number', 'scheduled_at', 'real_match_at', 'real_home_team', 'real_away_team', 'competition'])]
 class SeasonRound extends Model
@@ -17,5 +18,10 @@ class SeasonRound extends Model
     public function season(): BelongsTo
     {
         return $this->belongsTo(Season::class);
+    }
+
+    public function realMatch(): HasOne
+    {
+        return $this->hasOne(RealMatch::class);
     }
 }
