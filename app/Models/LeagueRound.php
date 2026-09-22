7306d457abdaacc3e8e0adf1bde240d221d0ef52
MolaQ <marcin.molak@gmail.com>
2026-09-22 11:06:48 +0200

    feat: Add real score fields to LeagueRound model fillable attributes

--- app/Models/LeagueRound.php
+++ app/Models/LeagueRound.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-#[Fillable(['season_league_id', 'round_number', 'scheduled_at', 'real_match_at', 'real_home_team', 'real_away_team', 'competition'])]
+#[Fillable(['season_league_id', 'round_number', 'scheduled_at', 'real_match_at', 'real_home_team', 'real_away_team', 'competition', 'real_score_home', 'real_score_away'])]
 class LeagueRound extends Model
 {
     protected function casts(): array
@@ -23,5 +23,10 @@ class LeagueRound extends Model
     public function matches(): HasMany
     {
         return $this->hasMany(MatchGame::class);
+    }
+
+    public function bonusQuestions(): HasMany
+    {
+        return $this->hasMany(LeagueRoundBonusQuestion::class);
     }
 }
